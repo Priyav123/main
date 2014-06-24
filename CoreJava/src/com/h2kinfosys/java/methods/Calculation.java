6114ea39f3af124abb1c36d3a8f3c5edc71898ff
Jayram <jayram.h2kinfosys@gmail.com>
2014-06-23 21:50:54 -0400

    Update

--- CoreJava/src/com/h2kinfosys/java/methods/Calculation.java
+++ CoreJava/src/com/h2kinfosys/java/methods/Calculation.java
@@ -43,6 +43,10 @@ public class Calculation {
 	}*/
 	
 	public int addValue(int a , int b) {
+		System.out.println("a= "+ a);
+		a = a+10;
+		b = b+5;
+		
 		return a+b;
 	}
 	
@@ -56,15 +60,22 @@ public class Calculation {
 	
 	public static void main(String[] args) {
 		Calculation add = new Calculation(10, 20);
+		
+		int value = add.addValue(12, 12);
+		
+		int finalValue = value + 10;
+		
+		System.out.println("Finale Value =" + finalValue);
+		
 //		add.add();
-		int calulatedVale = add.addMe();
+		/*int calulatedVale = add.addMe();
 		int finalValue = calulatedVale + 10;
 		
 		System.out.println( finalValue);
 		
 		Calculation sub = new Calculation(10, 5);
 		sub.firstValue = 12;
-		
+	*/	
 	}
 
 }
